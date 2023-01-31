cc9f836299ba568cdc41a61be75401aa04626a36
Jesús Antonio Rozo <rozozapatajesus1997@gmail.com>
2023-01-31 09:59:08 -0500

    Implementing select image and display

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
@@ -30,6 +32,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('learnuploadimages app is running!');
+    expect(compiled.querySelector('button')?.textContent).toContain('Guardar imagen');
   });
+
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ɵɵsetComponentScope } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  image=FormControl;
+   archivo=new FormData();
+  previsualizacion:any =null;
   title = 'learnuploadimages';
+  /**
+   *
+   */
+  imagenBase64:string='';
+  constructor(private http:HttpClient) {
+    
+  }
+  changeImage(event:any){
+    const activoSeleccionado=event.target.files[0]
+    this.previsualizacion=event.target.files[0]
+    this.archivo.append("image",this.previsualizacion,this.previsualizacion.name)
+
+    const render= new FileReader();
+    render.readAsDataURL(activoSeleccionado);
+    render.onload=()=>{
+      this.imagenBase64=render.result as string;
+      console.log(this.imagenBase64)
+      console.log(this.archivo)
+      console.log('data para enviar al servidor')
+      console.log("image",this.previsualizacion,this.previsualizacion.name)
+    }
+  }
+  // guardarImagen(){
+  //  this.http.post("./assets/images/roles/",this.imagenBase64).subscribe(res=>console.log(res))
+  // }
 }
